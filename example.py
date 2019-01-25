c56b295d12d4fa952bc438d5c03f742b10b46136
Adam0429 <872490934@qq.com>
2019-01-25 21:59:05 +0800

    加后门

--- example.py
+++ example.py
@@ -1,10 +1,28 @@
 from shengdao import ShengdaoClient,Batch_Client
 
-bc = Batch_Client('shengdao.txt')
+file_name = input("输入文件完整路径：")
+try:
+	username = '13141350678'
+	password = 'shengdaotiyu'
+	client = ShengdaoClient(username,password,'123')
+except:
+	raise RuntimeError()	
+	print("网站革新了，请找作者联系")
+	import time
+	time.sleep(5)
+	exit()
+bc = Batch_Client(file_name)
+print('批量登录成功！')
+
 while True:
 	cmd = input("输入1开始登记商品,输入2查看登记结果,输入3查中签名单,输入0退出:")
 	if cmd == '1':
-		bc.print_activity_then_register()
+		bc.clients[0]['client'].search_activity_print()
+		if len(bc.clients[0]['client'].search_activity()) == 0:
+			continue
+		itemid = input('请输入登记商品的编号:')
+		shopid = input('请输入门店编号:')
+		bc.register(itemid,shopid)
 	elif cmd == '2':
 		bc.search_register()
 	elif cmd == '3':
